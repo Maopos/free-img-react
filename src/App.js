72265365f5e81ecf87aade705d7dfe63135d303f
maopos <mauricioposadamurillo@gmail.com>
2021-10-20 15:14:42 -0500

    Version 21.10.20

--- src/App.js
+++ src/App.js
@@ -8,29 +8,87 @@ function App() {
   const [busqueda, guardarBusqueda] = useState("");
   const [imagenes, guardarImagenes] = useState([]);
 
+  // Paginador
+  const [paginaActual, guardarPaginaActual] = useState(1);
+  const [totalPaginas, guardarTotalPaginas] = useState(1);
+
   useEffect(() => {
     const consultarApi = async () => {
       if (busqueda === "") {
         return;
       }
 
-      const imgPorPagina = 30;
+      const imgPorPagina = 20;
       const key = "18725707-9f054c6b941dad20ff223e2ed";
-      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imgPorPagina}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imgPorPagina}&page=${paginaActual}`;
 
       const respuesta = await axios.get(url);
 
       guardarImagenes(respuesta.data.hits);
+
+      // Calcular total paginas
+      const totalPaginas = Math.ceil(respuesta.data.totalHits / imgPorPagina);
+      guardarTotalPaginas(totalPaginas);
+
+      // Saltar arriba
+      const saltarArriba = document.querySelector(".App-header");
+      saltarArriba.scrollIntoView({ behavior: "smooth" });
     };
     consultarApi();
-  }, [busqueda]);
+  }, [busqueda, paginaActual]);
+
+  // Pagina Anterior
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaActual - 1;
+    if (nuevaPaginaActual === 0) {
+      return;
+    }
+    guardarPaginaActual(nuevaPaginaActual);
+  };
+
+  // Pagina Siguiente
+
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaActual + 1;
+
+    if (nuevaPaginaActual > totalPaginas) {
+      return;
+    }
+
+    guardarPaginaActual(nuevaPaginaActual);
+  };
 
   return (
     <div className="App">
-      <h1 className="App-header">Free Img</h1>
+      <h1 className="App-header">Descarga Imagenes Gratis</h1>
       <Form guardarBusqueda={guardarBusqueda}></Form>
       <div className="row justify-content-center">
         <ListaImagenes imagenes={imagenes}></ListaImagenes>
+
+        <div className="col">
+          {paginaActual === 1 ? null : (
+            <button
+              type="button"
+              className="btn btn-warning"
+              onClick={paginaAnterior}
+            >
+              &laquo; Anterior
+            </button>
+          )}
+
+          {paginaActual === totalPaginas ? null : (
+            <button
+              type="button"
+              className="btn btn-warning"
+              onClick={paginaSiguiente}
+            >
+              Siguiente &raquo;
+            </button>
+          )}
+        </div>
+        <br />
+        <br />
+        <br />
       </div>
     </div>
   );
--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -7,6 +7,13 @@ const Imagen = ({ imagen }) => {
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
       <div className="card">
         <img src={previewURL} alt={tags} className="card-img-top" />
+        <div className="card-body">
+          <p >❤️ {likes}</p>
+          <p >Views {views}</p>
+        </div>
+        <div className="card-footer">
+          <a href={largeImageURL} target="blank" className="btn btn-warning btn-block">Ver imagen</a>
+        </div>
       </div>
     </div>
   );
